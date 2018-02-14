a80e9a61392912d12c5f37a3b1448f0372d88546
Za Wilgustus <zancas@contains.io>
2018-02-13 21:01:54 -0700

    Increment towards defining the end state of pave.

--- containment/pave.py
+++ containment/pave.py
@@ -3,6 +3,9 @@
 
 Functions:
     pave:  Create and populate a new ~/.containment directory
+
+INVARIANT:  After pave runs the contents of the .containment directory are not
+modified by the containment pave or activate commands.
 """
 
 # If the wardrobe is assembled then the EXTERNALBASIS is
@@ -12,15 +15,27 @@ import pathlib
 WARDROBE = pathlib.Path(os.environ["HOME"]).joinpath(".containment")
 DEFAULTS = WARDROBE.joinpath("defaults")
 BASIS = DEFAULTS.joinpath("basis.json")
+EQUIPMENT = DEFAULTS.joinpath("equipment.json")
 STAGE = pathlib.Path(os.environ["PWD"]).joinpath(".containment")
 EXTERNALBASIS = ("ubuntu@sha256:d3fdf5b1f8e8a155c17d5786280af1f5a04c10e9514"
                  "5a515279cf17abdf0191f")
+EQUIPMENT_MANIFEST = '["vim", "ipython", "pytest"]'
 
+DOCKERFILE_TEMPLATE = """FROM {EXTERNALBASIS}"""
 
 def _assemble_default_wardrobe():
     DEFAULTS.mkdir(parents=True)
     BASIS.write_text( '"' + EXTERNALBASIS + '"' )
+    EQUIPMENT.write_text( EQUIPMENT_MANIFEST )
 
+
+def _write_entrypoint():
+    # Write the defaalt entrypoint into the personal directory.
+    pass
+
+
+def _compose_Dockerfile():
+    
 
 def pave():
     """
@@ -28,6 +43,7 @@ def pave():
       containment pave
     """
     if WARDROBE.is_dir():
+        print(WARDROBE.name)
         return
     else:  # This is the first use by this User!!
         _assemble_default_wardrobe()
