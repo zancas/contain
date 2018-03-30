49e7c64f5b9cba3719ea7485f928be720d7445f2
Za Wilgustus <zancas@contains.io>
2018-03-30 13:20:14 -0600

    start dumping most recent dockerfiles by timestamp

--- containment/activate.py
+++ containment/activate.py
@@ -9,6 +9,7 @@ Functions:
 import os
 import pathlib
 import subprocess
+import time
 
 from .pave import PROJECTS
 from .pave import pave_community
@@ -28,20 +29,21 @@ def activate(project: ProjectId = None):
     proj_path = _get_project_path(project)
     proj_name = proj_path.name
     print(proj_name)
-    proj_base = proj_path.joinpath('.containment').joinpath('base')
-    if not proj_base.is_file():
+    community_base = proj_path.joinpath('.containment').joinpath('base')
+    if not community_base.is_file():
         # Create the base file since it did not exist
         pave_community(proj_path)
-    base_string = proj_base.read_text()
+    base_string = community_base.read_text()
     if not PROJECTS.is_dir():
         pave_personal(proj_name)
-
-    personal_string = PROJECTS\
-                      .joinpath(proj_name)\
-                      .joinpath("personal").read_text()
+    personal_projdir = PROJECTS.joinpath(proj_name)
+    personal_prefs = personal_projdir.joinpath("personal")
+    personal_string = personal_prefs.read_text()
     dockerfiletext = '\n'.join([base_string, personal_string])
     print(dockerfiletext)
-    
+    most_recent_containment = personal_projdir.joinpath(
+                                str(time.time())[:10])
+    most_recent_containment.write_text(dockerfiletext)
 
 def _get_project_path(project: ProjectId):
     """Find the path of the project based on the project name."""
