e79c9a54d24a2ae041a7637f5710f6fc7d58974e
Za Wilcox <zancas@protonmail.com>
2018-11-03 12:49:04 -0600

    simplify

--- containment/test/test_cli/test_activate/conftest.py
+++ containment/test/test_cli/test_activate/conftest.py
@@ -6,16 +6,6 @@ import pytest
 from ....builder import CommandLineInterface
 
 
-def apply_operations(stop_index):
-    def actual_decorator(cli_cls):
-        raw_klass = cli_cls.__bases__[0]
-        print(id(raw_klass))
-        for stub in mockattributes[stop_index:]:
-            setattr(raw_klass, stub, mock.MagicMock(name=stub))
-        return raw_klass
-
-    return actual_decorator
-
 @pytest.fixture
 def ensureconfig_mockcli():
     print("ensureconfig_mockcli")
