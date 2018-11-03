6ce6edcf567988b3a83323a8513b7bf7d5eb51fb
Za Wilcox <zancas@protonmail.com>
2018-11-03 13:46:29 -0600

    wip

--- containment/test/test_cli/test_activate/test_ensure_config.py
+++ containment/test/test_cli/test_activate/test_ensure_config.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
 """Activation tests for contain."""
+import os
+from pathlib import Path
 import pytest
 from unittest import mock
 
 from ....builder import CommandLineInterface
 from ....cli.activate import activate as actfun
 from ....cli import activate
+from ....config import config
 
 
-def test_fixturization(ensureconfig_mockcli):
+def test_pave_community(ensureconfig_mockcli, tmpdir):
+    print(dir(config.project_config))
+    config.project_config.path = Path(os.path.join(tmpdir, ".containment"))
+    print(config.project_config.path)
     with mock.patch('containment.cli.activate.CommandLineInterface',
                     new=ensureconfig_mockcli) as CLIO,\
          mock.patch('containment.builder.context') as mcontext,\
          mock.patch('containment.builder.config') as mconfig:
-        print(CLIO.ensure_config)
-        print(CLIO.write_dockerfile)
+        # The following line causes the code to enter pave_community.
         mconfig.project_config.path.is_dir.return_value = \
             False
-        c = CLIO()
-        #print(dir(c))
-        c.ensure_config()
-        print((mcontext.method_calls))
-        print((mconfig.method_calls))
-        #print(dir(mcontext))
+        CLIO().ensure_config()
+    
